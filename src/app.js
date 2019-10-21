62e5e4c7e60451fd90a5337a815a6c4192c6a809
Willian Gaudencio de Rezende <wil-g2@hotmail.com>
2019-10-20 22:36:43 -0300

    chore: Initial commit

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import 'dotenv/config';
+
+import express from 'express'; 
+import routes from './routes';
+
+class App {
+  constructor() {   
+    this.init();
+  }
+
+  init(){
+    this.server = express(); 
+    this.middlewares();
+    this.routes();
+  };
+  
+  middlewares() {
+    this.server.use(express.json());    
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+}
+  
+export default new App().server;
