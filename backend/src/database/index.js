c6ede3e3f5da64304ebb9380d301dc4f0aa4d413
Willian Gaudencio de Rezende <wil-g2@hotmail.com>
2019-10-26 16:55:30 -0300

    chore: add mongo connect and dependencies

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 
 import User from '../app/models/User';
 import Student from '../app/models/Student';
@@ -12,6 +13,7 @@ const models = [User, Student, Plans, Enroll];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   init() {
@@ -20,6 +22,13 @@ class Database {
     models
       .map(model => model.init(this.connection))
       .map(model => model.associate && model.associate(this.connection.models));
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useFindAndModify: true,
+    });
   }
 }
 
